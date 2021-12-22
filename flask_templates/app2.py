8480592c9b3741e3089df53dbe29e01f575a9e6e
Wilson <wiston.98@hotmail.com>
2021-12-21 21:36:59 -0500

    correccion en plantillas

--- flask_templates/app2.py
+++ flask_templates/app2.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+  return "<h2>probando plantillas</h2>"
+
 @app.route("/pagina1")
 def pagina1():
   return render_template('pagina1.html')
